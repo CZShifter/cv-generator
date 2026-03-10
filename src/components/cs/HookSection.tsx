e88b2fded647065efe5be4440788da8abe471f37
Your NameTomas <you@example.comtomastippl@gmail.com>
2026-03-10 21:22:22 +0100

    oprava seo pro profese_2

--- src/components/cs/HookSection.tsx
+++ src/components/cs/HookSection.tsx
@@ -1,4 +1,5 @@
 ﻿import styles from '@/scss/HookSection.module.scss'
+import Link from "next/link";
 import { SITE_VERSION } from "@/config/site";
 
 export default function HookSection() {
@@ -7,7 +8,7 @@ export default function HookSection() {
       <div className={styles.HookWrapper}>
         <div className={styles.HookTitleWrapper}>
           <h2>Strukturovaný životopis online za pár minut!</h2>
-          <p>Vytvořit <a href="/cs/profese">profesionální životopis</a> nemusí být složité. Mnoho uchazečů se denně potýká s nepraktickými editory, nepřehlednými šablonami a nutností registrace do služeb, které slibují víc, než dodají. Naše aplikace vznikla jako odpověď na tyto problémy.</p>
+          <p>Vytvořit <Link href="/cs/profese">profesionální životopis</Link> nemusí být složité. Mnoho uchazečů se denně potýká s nepraktickými editory, nepřehlednými šablonami a nutností registrace do služeb, které slibují víc, než dodají. Naše aplikace vznikla jako odpověď na tyto problémy.</p>
           <p>Formulář je intuitivní a doplněný živým náhledem. Díky tomu na vás nečeká překvapení ve formě špatného rozložení stránky po stažení životopisu. Výsledkem je elegantní PDF připravené k odeslání.</p>
           <p>S naší aplikací ušetříte svůj čas i trpělivost. Nevyžadujeme žádnou registraci, neukládáme vaše osobní údaje a nepřekvapíme vás žádným předplatným ani skrytými poplatky. Moderní životopis si vygenerujete rychle a jednoduše jen za 89 Kč.</p>
         </div>       
@@ -28,6 +29,8 @@ export default function HookSection() {
     </section>
   )
 }
+
+
 
 
 
--- src/components/sk/HookSection.tsx
+++ src/components/sk/HookSection.tsx
@@ -1,4 +1,5 @@
 ﻿import styles from '@/scss/HookSection.module.scss'
+import Link from "next/link";
 import { SITE_VERSION } from "@/config/site";
 
 export default function HookSection() {
@@ -7,7 +8,7 @@ export default function HookSection() {
       <div className={styles.HookWrapper}>
         <div className={styles.HookTitleWrapper}>
           <h2>Štruktúrovaný životopis online za pár minút!</h2>
-          <p>Vytvoriť <a href="/sk/profese">profesionálny životopis</a> nemusí byť zložité. Mnoho uchádzačov denne zápasí s nepraktickými editormi, neprehľadnými šablónami a nutnosťou registrácie do služieb, ktoré sľubujú viac, než dodajú. Naša aplikácia vznikla ako odpoveď na tieto problémy.</p>
+          <p>Vytvoriť <Link href="/sk/profese">profesionálny životopis</Link> nemusí byť zložité. Mnoho uchádzačov denne zápasí s nepraktickými editormi, neprehľadnými šablónami a nutnosťou registrácie do služieb, ktoré sľubujú viac, než dodajú. Naša aplikácia vznikla ako odpoveď na tieto problémy.</p>
           <p>Formulár je intuitívny a doplnený živým náhľadom. Vďaka tomu na vás nečaká prekvapenie vo forme zlého rozloženia stránky po stiahnutí životopisu. Výsledkom je elegantné PDF pripravené na odoslanie.</p>
           <p>S našou aplikáciou ušetríte svoj čas aj trpezlivosť. Nevyžadujeme žiadnu registráciu, neukladáme vaše osobné údaje a neprekvapíme vás žiadnym predplatným ani skrytými poplatkami. Moderný životopis si vygenerujete rýchlo a jednoducho len za 4 €.</p>
         </div>       
@@ -28,6 +29,7 @@ export default function HookSection() {
     </section>
   )
 }
+
 
 
 
